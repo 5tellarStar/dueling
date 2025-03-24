991d9995105f2ac3de309ad81d1a00e25466f9a3
StellarStar <skold2@outlook.com>
2025-03-24 22:06:41 +0100

    made most of the movement

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -7,35 +7,72 @@ public class PlayerManager : MonoBehaviour
 
     public int HP;
     public int WeaponHP;
-    public float speed;
 
     public int leaning;
 
     public InputAction move;
     public InputAction lean;
+    public InputAction lunge;
 
+    private Animator animator;
+
+    private int walkIndex;
+    private int lungeIndex;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         InputActionMap playerMap = isPlayer1 ? InputSystem.actions.FindActionMap("Player1") : InputSystem.actions.FindActionMap("Player2");
         move = playerMap.FindAction("Move");
-        lean = playerMap.FindAction("lean");
+        lean = playerMap.FindAction("Lean");
+        lunge = playerMap.FindAction("Lunge");
+        animator = GetComponent<Animator>();
+        walkIndex = Animator.StringToHash("walk");
+        lungeIndex = Animator.StringToHash("lunge");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(move.ReadValue<float>() > 0)
+        if (isPlayer1)
         {
-            transform.position += Vector3.right*speed*Time.deltaTime;
+            if(move.ReadValue<float>() > 0)
+            {
+                animator.SetInteger(walkIndex, 1);
+            }
+            else if (move.ReadValue<float>() < 0)
+            {
+                animator.SetInteger(walkIndex, -1);
+            }
+            else
+            {
+                animator.SetInteger(walkIndex, 0);
+            }
         }
-        else if (move.ReadValue<float>() < 0)
+        else
         {
-            transform.position -= Vector3.right*speed * Time.deltaTime;
+            if (move.ReadValue<float>() > 0)
+            {
+                animator.SetInteger(walkIndex, -1);
+            }
+            else if (move.ReadValue<float>() < 0)
+            {
+                animator.SetInteger(walkIndex, 1);
+            }
+            else
+            {
+                animator.SetInteger(walkIndex, 0);
+            }
         }
 
         leaning = (int)lean.ReadValue<float>();
+        
 
+        animator.SetBool(lungeIndex,lunge.IsPressed());
 
+    }
+
+    public void MoveForAnimation(int pixels)
+    {
+        transform.position += Vector3.right * pixels * 0.03125f * (isPlayer1 ? 1 : -1);
     }
 }
