f602baad1c938bab8d7e5717acf86b09991b7368
StellarStar <skold2@outlook.com>
2025-03-24 15:26:32 +0100

    fix player 2 contorls added battle manager

---
+++ Assets/Scripts/BattleManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class BattleManager : MonoBehaviour
+{
+    [SerializeField] private PlayerManager player1;
+    [SerializeField] private PlayerManager player2;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
