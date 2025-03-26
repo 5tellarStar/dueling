67ff2a5177c229c05d38c2f1354f9feacf3da9e8
StellarStar <skold2@outlook.com>
2025-03-26 12:21:19 +0100

    made thrust attacks do damage when hit and made them blockable

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -4,9 +4,15 @@ using UnityEngine.InputSystem;
 public class PlayerManager : MonoBehaviour
 {
     public bool isPlayer1;
+    [SerializeField] private PlayerManager otherPlayer;
 
     public int HP;
     public int WeaponHP;
+
+    public Vector2Int blockingState;
+
+    public int highHitWidth;
+    public int lowHitWidth;
 
     public int leaning;
 
@@ -52,7 +58,7 @@ public class PlayerManager : MonoBehaviour
     {
         if (isPlayer1)
         {
-            if(moveInput.ReadValue<float>() > 0)
+            if (moveInput.ReadValue<float>() > 0)
             {
                 animator.SetInteger(walkIndex, 1);
             }
@@ -82,17 +88,21 @@ public class PlayerManager : MonoBehaviour
         }
 
         leaning = (int)leanInput.ReadValue<float>();
+
         
+        animator.SetBool(lungeIndex, lungeInput.IsPressed());
 
-        animator.SetBool(lungeIndex,lungeInput.IsPressed());
+        animator.SetBool(blockIndex, blockInput.IsPressed());
 
-        animator.SetBool(blockIndex,blockInput.IsPressed());
-
-        animator.SetInteger(verticalIndex,(int)verticalInput.ReadValue<float>());
+        animator.SetInteger(verticalIndex, (int)verticalInput.ReadValue<float>());
 
         if (thrustInput.WasPressedThisFrame())
         {
             animator.SetTrigger(thrustIndex);
+        }
+        else if(blockInput.IsPressed())
+        {
+            animator.ResetTrigger(thrustIndex);
         }
 
     }
@@ -100,5 +110,96 @@ public class PlayerManager : MonoBehaviour
     public void MoveForAnimation(int pixels)
     {
         transform.position += Vector3.right * pixels * 0.03125f * (isPlayer1 ? 1 : -1);
+    }
+
+    public void SetBlockState(int state)
+    {
+        switch (state)
+        {
+            case 0:
+                blockingState = new Vector2Int(-1, 1);
+                break;
+            case 1:
+                blockingState = new Vector2Int(0, 1);
+                break;
+            case 2:
+                blockingState = new Vector2Int(1, 1);
+                break;
+            case 3:
+                blockingState = new Vector2Int(-1, 0);
+                break;
+            case 4:
+                blockingState = new Vector2Int(0, 0);
+                break;
+            case 5:
+                blockingState = new Vector2Int(1, 0);
+                break;
+            case 6:
+                blockingState = new Vector2Int(-1, -1);
+                break;
+            case 7:
+                blockingState = new Vector2Int(0, -1);
+                break;
+            case 8:
+                blockingState = new Vector2Int(1, -1);
+                break;
+        }
+    }
+
+    public void SetHighHitWidth(int pixels)
+    {
+        highHitWidth = pixels;
+    }
+
+    public void SetLowHitWidth(int pixels)
+    {
+        lowHitWidth = pixels;
+    }
+
+    public void Attack(string StateAndRange)
+    {
+        int stateInt = int.Parse(StateAndRange.Remove(1));
+        Vector2Int state = Vector2Int.zero;
+        switch (stateInt)
+        {
+            case 0:
+                state = new Vector2Int(-1, 1);
+                break;
+            case 1:
+                state = new Vector2Int(0, 1);
+                break;
+            case 2:
+                state = new Vector2Int(1, 1);
+                break;
+            case 3:
+                state = new Vector2Int(-1, 0);
+                break;
+            case 4:
+                state = new Vector2Int(0, 0);
+                break;
+            case 5:
+                state = new Vector2Int(1, 0);
+                break;
+            case 6:
+                state = new Vector2Int(-1, -1);
+                break;
+            case 7:
+                state = new Vector2Int(0, -1);
+                break;
+            case 8:
+                state = new Vector2Int(1, -1);
+                break;
+        }
+        int range = int.Parse(StateAndRange.Remove(0,2));
+
+        int distInPixels = (int)(Mathf.Abs(transform.position.x - otherPlayer.transform.position.x) / 0.03125f);
+
+        if((distInPixels < (range + otherPlayer.highHitWidth) && state.y == 1) || (distInPixels < (range + otherPlayer.lowHitWidth) && state.y == -1))
+        {
+            if(state != otherPlayer.blockingState)
+            {
+                otherPlayer.HP -= 1;
+            }
+        }
     }
 }
